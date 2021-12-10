cab8f344ec320a0f4613edb42d5e804545eb37b7
marcol9 <marekslipnickis@gmail.com>
2021-12-10 13:24:27 +0100

    changed ticket

--- pages/ticket/ticket.js
+++ pages/ticket/ticket.js
@@ -11,14 +11,12 @@ export default (projectionId) => {
     .then((mainHtml) => {
       content.innerHTML = mainHtml;
 
-      const table = document.querySelector("table");
       const submitBtn = document.querySelector(".submit-btn");
-      const h2ProjectionInfo = document.querySelector("h2");
 
       renderNavBar();
       renderFooter();
 
-      renderSeatsAndInfo(table, h2ProjectionInfo);
+      renderSeatsAndInfo();
       submitBtn.addEventListener("click", () => {
         const selectedSeats = readTable();
         sendRequestToBook(projectionId, selectedSeats);
@@ -28,7 +26,6 @@ export default (projectionId) => {
 
 let rows = 0;
 let columns = 0;
-
 
 function getProjectionInfo(responseObject) {
   //returns object with projction info
@@ -49,7 +46,9 @@ function renderProjectionInfo(responseObject, h2ProjectionInfo) {
     "Cinema hall: " + cinemaHallName + "<br> Movie name: " + movieName;
 }
 
-function renderSeatsAndInfo(table, h2ProjectionInfo) {
+function renderSeatsAndInfo() {
+  const table = document.querySelector("table");
+  const h2ProjectionInfo = document.querySelector("h4");
   //fetch seat and projection info from api and render data
   fetch(apiUrl)
     .then((response) => response.json())
@@ -63,32 +62,45 @@ function renderSeatsAndInfo(table, h2ProjectionInfo) {
 
 function sendRequestToBook(projectionId, selectedSeats) {
   //sends api request to book a seat
-  //const userJWTToken = JSON.parse(localStorage.getItem("user"));
-  //console.log("jwt token " + userJWTToken);
-  const numberOfSeats = selectedSeats.length;
-  for (let i = 0; i < numberOfSeats; i++) {
-    const column = selectedSeats[i].column;
-    const row = selectedSeats[i].row;
+  const userJWTToken = JSON.parse(localStorage.getItem("user"));
+  if (userJWTToken != null) {
+    const numberOfSeats = selectedSeats.length;
+    for (let i = 0; i < numberOfSeats; i++) {
+      const column = selectedSeats[i].column;
+      const row = selectedSeats[i].row;
 
-    const apiUrlPut =
-      "http://54.227.55.197/api/ticket/book/" +
-      projectionId +
-      "/row/" +
-      row +
-      "/column/" +
-      column;
+      const apiUrlPut =
+        "http://54.227.55.197/api/ticket/book/" +
+        projectionId +
+        "/row/" +
+        row +
+        "/column/" +
+        column;
 
-    fetch(apiUrlPut, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        // attaching the JWT token to the request
-
-        //Authorization: "Bearer " + userJWTToken.accessToken,
-      },
-    }).then(() => {
-      location.reload(); //reloads the page so that seat map is updated
-    });
+      fetch(apiUrlPut, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          // attaching the JWT token to the request
+          Authorization: "Bearer " + userJWTToken.accessToken,
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            location.reload(); //reloads the page so that seat map is updated
+          }
+        })
+        .catch((error) => {
+          if (response.status == 401) {
+            alert("Please log in in order to book a ticket");
+          } else {
+            alert("Strange error occured, try again");
+          }
+        });
+    }
+  } else {
+    alert("Please log in in order to book a ticket");
+    window.location.href = "/#/login";
   }
 }
 function clearTable() {
